b7c0b4a2a4f987937f90fd3834bf2e1bb50100c3
tuanluc97 <tuanluc50613@gmail.com>
2023-06-03 11:58:47 +0700

    ff

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from settings import *
 from sprite import *
 
+
 class Game:
     def __init__(self):
         # initialize game window, etc
@@ -16,8 +17,15 @@ class Game:
     def new(self):
         # start a new game
         self.all_sprites = pygame.sprite.Group()
-        self.player = Player()
+        self.platforms = pygame.sprite.Group()
+        self.player = Player(self)
         self.all_sprites.add(self.player)
+
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+
         g.run()
 
     def run(self):
@@ -33,6 +41,13 @@ class Game:
     def update(self):
         # Game loop - Update
         self.all_sprites.update()
+        #  check if player hits a platform
+        # if self.player.vel.y > 0
+        if self.player.vel.y > 0:
+            hits = pygame.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
 
     def event(self):
         for event in pygame.event.get():
@@ -40,15 +55,22 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
 
     def draw(self):
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
         pygame.display.flip()
+
     def show_start_screen(self):
         pass
+
     def show_go_screen(self):
         pass
+
+
 g = Game()
 g.show_start_screen()
 while g.running:
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
 TITLE = "new game"
 
 YELLOW = (255, 255, 0)
@@ -12,3 +12,10 @@ BLUE = (0, 0, 255)
 
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = 0.12
+PLAYER_GRAV = 0.8
+
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20)]
