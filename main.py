fc9a803e5289009813b436e20e1454966dc2d4bf
tuanluc97 <tuanluc50613@gmail.com>
2023-06-04 20:21:06 +0700

    next

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 import random
 from settings import *
 from sprite import *
@@ -6,53 +6,92 @@ from sprite import *
 class Game:
     def __init__(self):
         # initialize game window, etc
-        pygame.init()
-        pygame.mixer.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption(TITLE)
-        self.clock = pygame.time.Clock()
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
         self.running = True
 
     def new(self):
         # start a new game
-        self.all_sprites = pygame.sprite.Group()
-        self.player = Player()
+        self.all_sprites = pg.sprite.Group()
+        # self.platforms = pg.sprite.Group()
+        self.player = Player(self)
         self.all_sprites.add(self.player)
-        g.run()
+        # for plat in PLATFORM_LIST:
+        #     p = Platform(*plat)
+        #     self.all_sprites.add(p)
+        #     self.platforms.add(p)
+        self.run()
 
     def run(self):
-        # Game loop
-
+        # Game Loop
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
-            self.event()
+            # self.events()
             self.update()
             self.draw()
 
     def update(self):
-        # Game loop - Update
+        # Game Loop - Update
         self.all_sprites.update()
+        # check if player hits a platform - only if falling
+        # if self.player.vel.y > 0:
+        #     hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+        #     if hits:
+        #         self.player.pos.y = hits[0].rect.top
+        #         self.player.vel.y = 0
+        # if player reaches top 1/4 of screen
+        self.player.move()
+        # if self.player.rect.top <= HEIGHT / 4:
+        #     self.player.pos.y += abs(self.player.vel.y)
+        #     for plat in self.platforms:
+        #         plat.rect.y += abs(self.player.vel.y)
+                # if plat.rect.top >= HEIGHT:
+                    # plat.kill()
 
-    def event(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
+        # spawn new platforms to keep same average number
+        # while len(self.platforms) < 6:
+        #     width = random.randrange(50, 100)
+        #     p = Platform(random.randrange(0, WIDTH - width),
+        #                  random.randrange(-75, -30),
+        #                  width, 20)
+        #     self.platforms.add(p)
+        #     self.all_sprites.add(p)
 
+    # def events(self):
+        # Game Loop - events
+        # for event in pg.event.get():
+        #     # check for closing window
+        #     if event.type == pg.QUIT:
+        #         if self.playing:
+        #             self.playing = False
+        #         self.running = False
+        #     if event.type == pg.KEYDOWN:
+        #         if event.key == pg.K_SPACE:
+        #             self.player.jump()
+    # pass
     def draw(self):
+        # Game Loop - draw
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
-        pygame.display.flip()
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
     def show_start_screen(self):
+        # game splash/start screen
         pass
+
     def show_go_screen(self):
+        # game over/continue
         pass
+
 g = Game()
 g.show_start_screen()
 while g.running:
     g.new()
     g.show_go_screen()
 
-pygame.quit()
+pg.quit()
--- sprite.py
+++ sprite.py
@@ -1,28 +1,69 @@
-import pygame as pg
+# Sprite classes for platform game
+import pygame as pg, random, math
 from settings import *
 
 vec = pg.math.Vector2
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30, 40))
+        self.game = game
+        self.image = pg.Surface((40, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
-        self.vel = vec(0, 0)
-        self.acc = vec(0, 0)
+        self.speed = 2
+        self.gocDiChuyen = self.changeHuongDi()
+
+    def changeHuongDi(self):
+        mylist = [1, 2, 3, 4, 5, 6]
+        myRan = random.choices(mylist, weights=[1, 4, 1, 1, 4, 1], k=1)
+        gocDiChuyenRandom = 0
+        if myRan[0] == 5:
+            gocDiChuyenRandom = random.randint(-60, -20)
+        elif myRan[0] == 2:
+            gocDiChuyenRandom = random.randint(20, 60)
+        elif myRan[0] == 3:
+            gocDiChuyenRandom = random.randint(61, 90)
+        elif myRan[0] == 1:
+            gocDiChuyenRandom = random.randint(0, 19)
+        elif myRan[0] == 4:
+            gocDiChuyenRandom = random.randint(-19, 0)
+        elif myRan[0] == 6:
+            gocDiChuyenRandom = random.randint(-90, -61)
+        return gocDiChuyenRandom
+
+    def move(self):
+        self.pos.x += self.speed
+        self.pos.y += math.tan(math.radians(self.gocDiChuyen)) * self.speed
 
     def update(self):
-        self.acc = (0, 0)
-        keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT]:
-            self.acc.x = -0.5
-        if keys[pg.K_RIGHT]:
-            self.acc.y = 0.5
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+
+        # apply friction
+        # self.acc.x += self.vel.x * PLAYER_FRICTION
+        # # equations of motion
+        # self.vel += self.acc
+        # self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH - 30:
+            self.gocDiChuyen = self.changeHuongDi()
+        if self.pos.x < 20:
+            self.gocDiChuyen = self.changeHuongDi()
+        if self.pos.y > HEIGHT - 30:
+            self.gocDiChuyen = self.changeHuongDi()
+        if self.pos.y < 20:
+            self.gocDiChuyen = self.changeHuongDi()
 
         self.rect.center = self.pos
+
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
